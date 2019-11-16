bbf808635ef794a5b71bf35c405216e45dfdddbb
Ubuntu <ubuntu@ip-172-31-81-162.ec2.internal>
2019-11-16 00:55:16 +0000

    Countries being displayed in a table

--- world.php
+++ world.php
@@ -11,12 +11,28 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo $country;
 
 ?>
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= $row['name']; ?></td>
+            <td><?= $row['continent']; ?></td>
+            <td><?= $row['head_of_state']; ?></td>
+            <td><?= $row['independence_year']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+
